8ff8af1865200206d80ffd2810c9390228e30393
lnccccc <601452185@qq.com>
2018-11-05 15:37:08 +0800

    前端

--- work_flow/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import orders_list,order_stat,charge_person
-admin.site.register(orders_list)
-admin.site.register(order_stat)
-admin.site.register(charge_person)
-# Register your models here.
--- work_flow/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WrokFlowConfig(AppConfig):
-    name = 'work_flow'
