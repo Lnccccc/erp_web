cad60822948beeb677522e6321c9b550aad96a51
lnccccc <601452185@qq.com>
2018-11-07 18:05:55 +0800

    重做下单页面

--- work_flow/forms.py
+++ work_flow/forms.py
@@ -3,22 +3,19 @@ from django.contrib.admin import widgets
 from .models import orders_list
 from datetime import datetime
 class WorkFlowForm(forms.ModelForm):
-    order_time = forms.DateField()
-    sub_time = forms.DateField(widget=widgets.AdminDateWidget())
-    order_time.widget.attrs.update({'value':str(datetime.now())})
-    client = forms.CharField(max_length=256)
-    order_quantity = forms.IntegerField()
-    spec = forms.CharField(max_length=256)
-    unit = forms.CharField(max_length=256)
-    person_incharge = forms.CharField(max_length=256)
-    client.widget.attrs.update({'class':'form-control','id':'focusedInput'})
-    order_quantity.widget.attrs.update({'class':'form-control','id':'focusedInput'})
-    spec.widget.attrs.update({'class':'form-control','id':'focusedInput'})
-    unit.widget.attrs.update({'class':'form-control','id':'focusedInput'})
-    person_incharge.widget.attrs.update({'class':'form-control','id':'focusedInput'})
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['client'].widget.attrs.update({'class':'form-control','id':'focusedInput'})
+        self.fields['order_quantity'].widget.attrs.update({'class':'form-control','id':'focusedInput'})
+        self.fields['spec'].widget.attrs.update({'class':'form-control','id':'focusedInput'})
+        self.fields['unit'].widget.attrs.update({'class':'form-control','id':'focusedInput'})
+        self.fields['person_incharge'].widget.attrs.update({'class':'form-control','id':'focusedInput'})
 
     class Meta:
         model=orders_list
+        fields = ('client','order_quantity','spec','unit','person_incharge')
 
-
+    order_time = forms.DateField()
+    sub_time = forms.DateField(widget=widgets.AdminDateWidget())
+    order_time.widget.attrs.update({'value':str(datetime.now())})
 
