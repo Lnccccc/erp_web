24d243c2798aaa74c38efe214f34ce6f4d2cd176
Lnccccc <601452185@qq.com>
2018-11-08 09:01:17 +0800

    Update form.py

--- account/form.py
+++ account/form.py
@@ -2,27 +2,27 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Profile,WeixinUser
 
-class LoginForm(forms.Form):
-    username= forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+# class LoginForm(forms.Form):
+#     username= forms.CharField()
+#     password = forms.CharField(widget=forms.PasswordInput)
 
-class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password',widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password',widget=forms.PasswordInput)
+# class UserRegistrationForm(forms.ModelForm):
+#     password = forms.CharField(label='Password',widget=forms.PasswordInput)
+#     password2 = forms.CharField(label='Repeat password',widget=forms.PasswordInput)
 
-    class Meta:
-        model = User
-        fields = ('username','first_name','email')
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
-            raise forms.ValidationError("Passwords don't match")
-        return cd['password2']
+#     class Meta:
+#         model = User
+#         fields = ('username','first_name','email')
+#     def clean_password2(self):
+#         cd = self.cleaned_data
+#         if cd['password'] != cd['password2']:
+#             raise forms.ValidationError("Passwords don't match")
+#         return cd['password2']
 
 class WxUserEditForm(forms.ModelForm):
     class Meta:
         fields = WeixinUser
-        fields=('nickname',)
+        fields='__all__'
 
 
 class ProfileEditForm(forms.ModelForm):
@@ -32,7 +32,7 @@ class ProfileEditForm(forms.ModelForm):
         self.fields['real_name'].widget.attrs.update({'class':'form-control'})
     class Meta:
         model=Profile
-        fields=('company','real_name')
+        fields=['company','real_name']
 
 
 class SearchForm(forms.Form):
