f07432ddc4515f7922f881c22c6429fdd0f786bc
lnccccc <601452185@qq.com>
2018-11-12 13:55:20 +0800

    订单详情

--- work_flow/forms.py
+++ work_flow/forms.py
@@ -23,7 +23,22 @@ class WorkFlowForm(forms.ModelForm):
     sub_time.widget.attrs.update({'class':'form-control'})
 
 class WorkFlowDetailForm(forms.ModelForm):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['client'].widget.attrs.update({'class':'form-control','id':'focusedInput'})
+        self.fields['order_quantity'].widget.attrs.update({'class':'form-control','id':'focusedInput'})
+        self.fields['spec'].widget.attrs.update({'class':'form-control','id':'focusedInput'})
+        self.fields['unit'].widget.attrs.update({'class':'form-control','id':'focusedInput'})
+        self.fields['person_incharge'].widget.attrs.update({'class':'form-control','id':'focusedInput'})
+        self.fields['requirement'].widget.attrs.update({'class':'form-control','id':'focusedInput'})
+        self.fields['remark'].widget.attrs.update({'class':'form-control','id':'focusedInput'})
+
     class Meta:
-        model = orders_list
-        fields = '__all__'
+        model=orders_list
+        fields = ('client','order_quantity','spec','unit','person_incharge','requirement','remark')
+
+    order_time = forms.DateField()
+    sub_time = forms.DateField(widget=widgets.AdminDateWidget())
+    order_time.widget.attrs.update({'value':datetime.now().strftime('%Y-%m-%d'),'class':'form-control'})
+    sub_time.widget.attrs.update({'class':'form-control'})
 
