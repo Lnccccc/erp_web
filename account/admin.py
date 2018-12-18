7962b148092220c646a7826d62476570d2c86efa
lnccccc <601452185@qq.com>
2018-12-18 12:13:52 +0800

    新增company模型

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile,WeixinUser
+from .models import Profile,WeixinUser,Company
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(WeixinUser)
+admin.site.register(Company)
