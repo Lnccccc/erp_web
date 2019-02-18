5e7fb5604ed40aba1eba3944d82f9d3055f0eb86
lnccccc <601452185@qq.com>
2019-02-18 22:03:20 +0800

    migrations

--- erp/settings.py
+++ erp/settings.py
@@ -125,5 +125,5 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS= [os.path.join(BASE_DIR,'static'),'/var/static/',]
+STATICFILES_DIRS= [os.path.join(BASE_DIR,'static'),]
 STATIC_ROOT = "/var/static/"
